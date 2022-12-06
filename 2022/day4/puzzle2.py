46844b6e4cb25375113103f06ff96bed9c24b96e
larsver <lars.larsvermeulen@gmail.com>
2022-12-06 19:55:30 +0100

    Add day4 2022

---
+++ 2022/day4/puzzle2.py
@@ -0,0 +1,25 @@
+file = open("input.txt")
+
+oplossing = 0
+
+for l in file:
+    print(l)
+    help = (l.strip()).split(',')
+    print(help)
+    section1 = help[0].split('-')
+    section2 = help[1].split('-')
+    print(section1)
+    print(section2)
+
+    # if (int(section1[0]) >= int(section2[0]) and int(section1[1]) <= int(section2[1])): #section1 in section2
+    #     oplossing += 1
+    # elif (int(section2[0]) >= int(section1[0]) and int(section2[1]) <= int(section1[1])): #section2 in section1
+    #     oplossing += 1
+
+    if int(section2[0]) <= int(section1[1]) <= int(section2[1]):   
+        oplossing += 1
+    elif int(section1[0]) <= int(section2[1]) <= int(section1[1]):
+        oplossing += 1
+
+print(oplossing)
+file.close()
